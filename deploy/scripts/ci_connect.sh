3732d431eeaaca34ba50685f74592297342f664a
Kai Davenport <kaiyadavenport@gmail.com>
2018-07-25 23:13:23 +0100

    update name of service account token var and remove file after

--- deploy/scripts/ci_connect.sh
+++ deploy/scripts/ci_connect.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 set -e
 
-if [ -z "${GCLOUD_SERVICE_KEY}" ]; then
-  echo >&2 "GCLOUD_SERVICE_KEY needed"
+if [ -z "${GCLOUD_SERVICE_ACCOUNT_TOKEN}" ]; then
+  echo >&2 "GCLOUD_SERVICE_ACCOUNT_TOKEN needed"
   exit 1
 fi
 if [ -z "${GCP_PROJECT_ID}" ]; then
@@ -17,9 +17,10 @@ if [ -z "${GCP_CLUSTER_ID}" ]; then
   echo >&2 "GCP_CLUSTER_ID needed"
   exit 1
 fi
-echo $GCLOUD_SERVICE_KEY | base64 -d > ${HOME}/gcloud-service-key.json
+echo $GCLOUD_SERVICE_ACCOUNT_TOKEN | base64 -d > ${HOME}/gcloud-service-key.json
 echo "activating gcloud service account"
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+rm -f ${HOME}/gcloud-service-key.json
 echo "set gcloud project $GCP_PROJECT_ID"
 gcloud config set project $GCP_PROJECT_ID
 echo "connect to container cluster $GCP_CLUSTER_ID in $GCP_ZONE"
