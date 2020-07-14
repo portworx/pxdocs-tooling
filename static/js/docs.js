cd4f90a9bbe3e19c7933a5f9a98e53155e3be68d
Andrei Popescu <32929697+andreipope@users.noreply.github.com>
2020-07-14 23:04:58 +0300

    full screen search results (#60)
    full screen search results

--- static/js/docs.js
+++ static/js/docs.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
   /*
 
     style markdown components
@@ -16,11 +16,11 @@ $(function() {
       border: '1px solid #ccc'
     })
 
-  if(EQUALIZE_TABLE_WIDTHS) {
+  if (EQUALIZE_TABLE_WIDTHS) {
     $('.docs-content table td')
-    .css({
-      width: EQUALIZE_TABLE_WIDTHS
-    })
+      .css({
+        width: EQUALIZE_TABLE_WIDTHS
+      })
   }
 
 
@@ -30,7 +30,7 @@ $(function() {
 
   */
   function setupSidebarMenu() {
-    $('div[data-menu-type]').each(function() {
+    $('div[data-menu-type]').each(function () {
       var link = $(this)
       var type = link.attr('data-menu-type')
       var id = link.attr('data-menu-id')
@@ -38,19 +38,19 @@ $(function() {
 
       var childrenContent = $('#menu-children-' + id)
 
-      if(childrenContent.hasClass('open')) {
+      if (childrenContent.hasClass('open')) {
         link.find('.material-icons').html('keyboard_arrow_down')
       }
       else {
         link.find('.material-icons').html('keyboard_arrow_right')
       }
 
-      link.click(function(e) {
-        if(type == 'leaf') return
+      link.click(function (e) {
+        if (type == 'leaf') return
         e.preventDefault()
         e.stopPropagation()
 
-        if(childrenContent.hasClass('open')) {
+        if (childrenContent.hasClass('open')) {
           childrenContent.removeClass('open')
           link.find('.material-icons').html('keyboard_arrow_right')
         }
@@ -84,12 +84,12 @@ $(function() {
     var searchWords = highlightResult.matchedWords
 
     let processedText = (highlightResult.value || '')
-      .replace(/^.*?(<\w+)/, function(wholeMatch, chunk) {
+      .replace(/^.*?(<\w+)/, function (wholeMatch, chunk) {
         return chunk
       })
       .replace(/^.*?<\/li>/, '')
 
-    processedText = processedText.replace(/<img .*?>/g,'')
+    processedText = processedText.replace(/<img .*?>/g, '')
     processedText = '<div>' + processedText + '</div>'
 
     let text = $(processedText).text()
@@ -97,50 +97,50 @@ $(function() {
     var foundTerms = {}
 
     var resultMatches = searchWords
-      .filter(function(word) {
+      .filter(function (word) {
         return getMatchIndex(text, word) >= 0
       })
-      .map(function(word) {
+      .map(function (word) {
         var firstMatch = getMatchIndex(text, word)
 
         var startMatch = firstMatch - SEARCH_HIGHLIGHT_BLEED
-        if(startMatch < 0) startMatch = 0
+        if (startMatch < 0) startMatch = 0
 
         var endMatch = firstMatch + word.length + SEARCH_HIGHLIGHT_BLEED
-        if(endMatch > text.length-1) endMatch = text.length-1
+        if (endMatch > text.length - 1) endMatch = text.length - 1
 
         var textChunk = text.substring(startMatch, endMatch)
 
-        searchWords.forEach(function(word) {
+        searchWords.forEach(function (word) {
           textChunk = textChunk.replace(new RegExp(word, 'gi'), '<em>' + word + '</em>')
         })
         return '...' + textChunk + '...'
       })
-      .filter(function(match) {
+      .filter(function (match) {
         var matchingChunk = match.match(/\<.*\>/)[0]
-        if(!matchingChunk) return true
-        if(foundTerms[matchingChunk]) return false
+        if (!matchingChunk) return true
+        if (foundTerms[matchingChunk]) return false
         foundTerms[matchingChunk] = true
         return true
       })
 
-    if(resultMatches.length > 3) {
-      resultMatches = resultMatches.slice(0,3)
+    if (resultMatches.length > 3) {
+      resultMatches = resultMatches.slice(0, 3)
     }
 
     item._resultMatches = resultMatches.join('<br />')
     return item
   }
 
-  function makeConfiguration () {
+  function makeConfiguration() {
     return instantsearch.widgets.configure({
-      hitsPerPage: 10,
+      hitsPerPage: 100,
       attributesToRetrieve: ['title', 'keywords', 'objectID', 'sectionTitles', 'url', 'sectionURL', 'content'],
       attributesToHighlight: ['title', 'keywords', 'objectID', 'sectionTitles', 'url', 'sectionURL', 'content']
     })
   }
 
-  function makeSearchBox (containerName) {
+  function makeSearchBox(containerName) {
     return instantsearch.widgets.searchBox({
       container: `#${containerName}`,
       cssClasses: {
@@ -160,33 +160,33 @@ $(function() {
     })
   }
 
-  function makeHits (containerName) {
+  function makeHits(containerName) {
     return instantsearch.widgets.hits({
       escapeHTML: false,
       container: `#${containerName}`,
       templates: {
         empty: [
           '<div class="search-hits-empty">',
-              'no results',
+          'no results',
           '</div>',
         ].join("\n"),
         item: [
           '<div class="search-hits-row">',
-            '<div class="search-hits-section">',
-              '<a href="{{{sectionURL}}}" target="_blank">',
-                '{{{_highlightResult.sectionTitles.value}}}',
-              '</a>',
-            '</div>',
-            '<div class="search-hits-page">',
-              '<a href="{{{url}}}"  target="_blank">',
-                '{{{_highlightResult.title.value}}}',
-              '</a>',
-            '</div>',
-            '<div class="search-hits-matches">',
-              '<a href="{{{url}}}"  target="_blank">',
-                '{{{_resultMatches}}}',
-              '</a>',
-            '</div>',
+          '<div class="search-hits-section">',
+          '<a href="{{{sectionURL}}}" target="_blank">',
+          '{{{_highlightResult.sectionTitles.value}}}',
+          '</a>',
+          '</div>',
+          '<div class="search-hits-page">',
+          '<a href="{{{url}}}"  target="_blank">',
+          '{{{_highlightResult.title.value}}}',
+          '</a>',
+          '</div>',
+          '<div class="search-hits-matches">',
+          '<a href="{{{url}}}"  target="_blank">',
+          '{{{_resultMatches}}}',
+          '</a>',
+          '</div>',
           '</div>',
         ].join("\n")
       },
@@ -199,11 +199,11 @@ $(function() {
 
   function makeSecondaryIndex(/*indexName, hitsContainerName*/ productNameAndIndex) {
     return instantsearch.widgets
-    .index({ indexName: productNameAndIndex.indexName })
-    .addWidgets([
-      makeConfiguration(),
-      makeHits(`search-hits-${productNameAndIndex.productName}`),
-    ])
+      .index({ indexName: productNameAndIndex.indexName })
+      .addWidgets([
+        makeConfiguration(),
+        makeHits(`search-hits-${productNameAndIndex.productName}`),
+      ])
   }
 
   function setupAlgolia() {
@@ -212,7 +212,7 @@ $(function() {
       routing: true,
       indexName: productNamesAndIndices[0].indexName,
       searchClient,
-      searchFunction: function(helper) {
+      searchFunction: function (helper) {
         var searchResults = $('#search-wrapper');
         if (helper.state.query === '') {
           searchResults.hide()
@@ -228,35 +228,35 @@ $(function() {
       makeSearchBox('search-box'),
       makeHits(`search-hits-${productNamesAndIndices[0].productName}`),
     ])
-    for (currentIndex = 1 ; currentIndex < productNamesAndIndices.length; currentIndex ++) {
+    for (currentIndex = 1; currentIndex < productNamesAndIndices.length; currentIndex++) {
       search.addWidgets([makeSecondaryIndex(productNamesAndIndices[currentIndex])])
     }
 
-  search.start()
+    search.start()
 
-  $('#search-container').show()
-  $('#search-container-holding-space').hide()
+    $('#search-container').show()
+    $('#search-container-holding-space').hide()
 
   }
 
 
-  if(ALGOLIA_APP_ID && ALGOLIA_API_KEY && PRODUCT_NAMES_AND_INDICES && $('#search-box').length >= 1) {
+  if (ALGOLIA_APP_ID && ALGOLIA_API_KEY && PRODUCT_NAMES_AND_INDICES && $('#search-box').length >= 1) {
     setupAlgolia()
   }
 
   $(document).keyup(function (e) {
-    if ($('.ais-SearchBox-input:focus') && $('.ais-SearchBox-input').val().length > 0 && (e.keyCode === 13)  && !$('#search-hits').hasClass('full-screen')) {
+    if ($('.ais-SearchBox-input:focus') && $('.ais-SearchBox-input').val().length > 0 && (e.keyCode === 13) && !$('#search-hits').hasClass('full-screen')) {
       $('#search-wrapper, .docs-drawer').addClass('full-screen')
       $('.docs-navigation, .version-menu, .docs-content, #scrollspy-container, .docs-footer-padding, .docs-footer').hide()
-      $('#search-hits li').addClass('show-li')
+      $('.search-hits li').addClass('show-li')
       $('#search-box').prepend('<a href="#" class="full-screen__close"><i class="material-icons">close</i><br/>Close</a>')
     }
   })
 
 
-  $('body').on('click', '.full-screen__close', function() {
+  $('body').on('click', '.full-screen__close', function () {
     $('#search-wrapper, .docs-drawer').removeClass('full-screen')
-    $('#search-hits li').removeClass('show-li')
+    $('.search-hits li').removeClass('show-li')
     $('.docs-navigation, .version-menu, .docs-content, #scrollspy-container, .docs-footer-padding, .docs-footer').show()
     $(this).remove()
   })
@@ -269,11 +269,11 @@ $(function() {
 
   function activateVersionMenu() {
     var allVersions = VERSIONS_ALL.split(',')
-    allVersions.forEach(function(version) {
+    allVersions.forEach(function (version) {
       var versionOption = $('<li class="mdl-menu__item">Version ' + version + '</li>')
 
-      versionOption.click(function() {
-        if(version == VERSIONS_CURRENT) return
+      versionOption.click(function () {
+        if (version == VERSIONS_CURRENT) return
         var url = location.protocol + '//' + version + '.' + VERSIONS_BASE_URL
         document.location = url
       })
@@ -283,7 +283,7 @@ $(function() {
     $('#version-menu #text-button').text('Version: ' + VERSIONS_CURRENT)
   }
 
-  if(VERSIONS_BASE_URL) {
+  if (VERSIONS_BASE_URL) {
     activateVersionMenu()
     $('#version-menu-dropdown').css({
       visibility: 'visible'
@@ -308,7 +308,7 @@ $(function() {
   var scrollWindow = $(window)
 
   function highlightScrollLink(id) {
-    if(!id) return
+    if (!id) return
     $('.scrollspy-item').removeClass('active')
     $('#scrollspy-menu-' + id.replace('#', '')).addClass('active')
   }
@@ -319,7 +319,7 @@ $(function() {
 
     var windowScroll = scrollWindow.scrollTop()
 
-    if(clickLinkHash) {
+    if (clickLinkHash) {
       clickLinkHash = null
       return
     }
@@ -329,18 +329,18 @@ $(function() {
 
     var elementsAboveFold = []
 
-    for(var i=0; i<allElements.length; i++) {
+    for (var i = 0; i < allElements.length; i++) {
       var headerItem = allElements.eq(i)
       var headerItemPosition = headerItem.offset().top - windowScroll
 
-      if(headerItemPosition < SCROLLSPY_ACTIVE_BUFFER) {
+      if (headerItemPosition < SCROLLSPY_ACTIVE_BUFFER) {
         elementsAboveFold.push(headerItem)
       }
     }
 
-    var activeElement = elementsAboveFold.length > 0 ? elementsAboveFold[elementsAboveFold.length-1] : null
+    var activeElement = elementsAboveFold.length > 0 ? elementsAboveFold[elementsAboveFold.length - 1] : null
 
-    if(!activeElement) {
+    if (!activeElement) {
       $('.scrollspy-item').removeClass('active')
       return
     }
@@ -350,10 +350,10 @@ $(function() {
     const newHashId = activeElement.attr('id')
     const currentHashId = window.location.hash.replace('#', '')
 
-    if(newHashId && newHashId != currentHashId) {
+    if (newHashId && newHashId != currentHashId) {
 
       // disable history push state for right side menu items
-      if(history.pushState) {
+      if (history.pushState) {
         // history.pushState(null, null, '#' + newHashId)
       }
       else {
@@ -364,26 +364,26 @@ $(function() {
 
   function setupInitialScrollPosition() {
     const currentHashId = window.location.hash.replace('#', '')
-    if(!currentHashId) return
+    if (!currentHashId) return
     const headerItem = $('#' + currentHashId)
-    if(headerItem.length <= 0) return
+    if (headerItem.length <= 0) return
     scrollWindow.scrollTop(headerItem.offset().top - 140)
   }
 
-  scrollWindow.scroll(function() {
+  scrollWindow.scroll(function () {
     checkScrollPositions()
   })
 
   function setupScrollspyMenu() {
-    if(scrollspyContainer.length <= 0) return
+    if (scrollspyContainer.length <= 0) return
 
-    pageContent.find(SCROLLSPY_HEADER_SELECTOR).each(function() {
+    pageContent.find(SCROLLSPY_HEADER_SELECTOR).each(function () {
       var headerItem = $(this)
       var scrollItem = $('<li></li>')
       var scrollItemLink = $('<a href="#' + headerItem.attr('id') + '">' + headerItem.text() + '</a>')
 
-      scrollItemLink.click(function(e) {
-        setTimeout(function() {
+      scrollItemLink.click(function (e) {
+        setTimeout(function () {
           clickLinkHash = headerItem.attr('id')
           highlightScrollLink(clickLinkHash)
           scrollWindow.scrollTop(headerItem.offset().top - 140)
@@ -397,7 +397,7 @@ $(function() {
       scrollspyList.append(scrollItem)
     })
 
-    pageContent.find("h2,h3,h4,h5,h6").each(function() {
+    pageContent.find("h2,h3,h4,h5,h6").each(function () {
       var headerItem = $(this)
       var headerLink = $('<a></a>')
       headerLink.attr({
@@ -430,7 +430,7 @@ $(function() {
     var documentHeight = scrollWindow.height()
 
     // give the foot a top-margin for short documents
-    if(footerOffset < documentHeight) {
+    if (footerOffset < documentHeight) {
       var footerHeightOffset = documentHeight - footerOffset
       footerElemPadding.css({
         height: footerHeightOffset + 'px',
@@ -445,27 +445,27 @@ $(function() {
     copy paste
 
   */
-  $('.highlight').each(function(i) {
+  $('.highlight').each(function (i) {
     var highlightElem = $(this)
     var codeElem = highlightElem.find('pre code.language-text');
     codeElem.parent().parent().addClass('copyable');
     highlightElem.find('pre code.language-output').parent().parent().addClass('typeOutput');
     var copyDiv = $([
       '<div class="copy-link">',
-        '<button id="clipboard-icon-' + i + '" class="button">',
-          '<i class="material-icons">assignment</i>',
+      '<button id="clipboard-icon-' + i + '" class="button">',
+      '<i class="material-icons">assignment</i>',
 
-        '</button>',
-        '<div class="mdl-tooltip mdl-tooltip--small" for="clipboard-icon-' + i + '">',
-        'copy to clipboard',
-        '</div>',
+      '</button>',
+      '<div class="mdl-tooltip mdl-tooltip--small" for="clipboard-icon-' + i + '">',
+      'copy to clipboard',
+      '</div>',
       '</div>'
     ].join("\n"))
 
     highlightElem.prepend(copyDiv)
 
 
-    copyDiv.click(function() {
+    copyDiv.click(function () {
 
       var $temp = $("<textarea>" + codeElem.text() + "</textarea>")
       $("body").append($temp)
@@ -477,7 +477,7 @@ $(function() {
         .find('button')
         .addClass('mdl-button--primary active').parent().addClass('active');
 
-      setTimeout(function() {
+      setTimeout(function () {
         copyDiv
           .find('button')
           .removeClass('mdl-button--primary active').parent().removeClass('active');
@@ -497,27 +497,27 @@ $(function() {
     var linkWidth = link.width()
     var menuWidth = menu.width()
     menu.css({
-      left: position.left + (linkWidth/2) - (menuWidth/2),
+      left: position.left + (linkWidth / 2) - (menuWidth / 2),
       top: position.top + 30,
     })
     menu.show()
   }
 
   function setupDropDownMenus() {
-    $('a[data-dropdown]').each(function() {
+    $('a[data-dropdown]').each(function () {
       var menuLink = $(this)
       var menuId = menuLink.attr('data-dropdown')
       var menu = $('#' + menuId)
 
-      menu.click(function(e) {
+      menu.click(function (e) {
         e.stopPropagation()
       })
 
-      menuLink.click(function(e) {
+      menuLink.click(function (e) {
         e.preventDefault()
         e.stopPropagation()
 
-        if(menu.is(':visible')) {
+        if (menu.is(':visible')) {
           menu.hide()
         }
         else {
@@ -528,17 +528,17 @@ $(function() {
       })
     })
 
-    $('body').click(function() {
+    $('body').click(function () {
       $('.dropdown-menu').hide()
     })
 
-    $(window).resize(function() {
-      $('a[data-dropdown]').each(function() {
+    $(window).resize(function () {
+      $('a[data-dropdown]').each(function () {
         var menuLink = $(this)
         var menuId = menuLink.attr('data-dropdown')
         var menu = $('#' + menuId)
 
-        if(menu.is(':visible')) {
+        if (menu.is(':visible')) {
           showDropDownMenu(menu)
         }
       })
@@ -554,8 +554,8 @@ $(function() {
   */
   var docsDrawer = $('.docs-drawer')
   function setupSidebarToggleButton() {
-    $('#toggle-sidebar-button').click(function() {
-      if(docsDrawer.hasClass('visible')) {
+    $('#toggle-sidebar-button').click(function () {
+      if (docsDrawer.hasClass('visible')) {
         docsDrawer.removeClass('visible')
       }
       else {
@@ -571,7 +571,7 @@ $(function() {
     sidebar scroll
 
   */
-  $(window).scroll( function() {
+  $(window).scroll(function () {
     var headerHeight = $('.docs-header').height()
     var windowHeight = $(document).height()
     var scrolledVal = $(document).scrollTop().valueOf()
@@ -596,7 +596,7 @@ $(function() {
 
   */
 
-  $('body').on('click', '.btn-orange', function() {
+  $('body').on('click', '.btn-orange', function () {
     $('.form').toggle('slow')
     if ($(this).text() == 'Contact') {
       $(this).text('Close')
@@ -609,7 +609,7 @@ $(function() {
     open external links target="_blank"
 
   */
-  $(document.links).filter(function() {
+  $(document.links).filter(function () {
     return this.hostname != window.location.hostname
   }).attr('target', '_blank')
   /*
@@ -617,7 +617,7 @@ $(function() {
     full screen iframe
 
   */
-  $('body').on('click', '.full-screen__link', function() {
+  $('body').on('click', '.full-screen__link', function () {
     var windowHeight = $(document).height()
     var iframeHeight = windowHeight - 113
     $('.full-screen__iframe-wrap').css('height', iframeHeight)
@@ -625,7 +625,7 @@ $(function() {
     $('.docs-footer').hide()
   })
 
-  $('body').on('click', '.full-screen__iframe__close', function() {
+  $('body').on('click', '.full-screen__iframe__close', function () {
     $('.full-screen__iframe-wrap, .full-screen__iframe__close').hide()
     $('.docs-footer').show()
   })
