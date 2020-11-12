680aacccb062385e523e7e2b28f25a09efa0f754
Andrei Popescu <andrei.popescu@gmail.com>
2020-11-12 17:45:47 +0200

    Init
    Init

---
+++ build/before_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# update gcloud apt source
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+# install docker-ce and kubectl
+sudo apt-get update -yq
+sudo apt-get -o Dpkg::Options::="--force-confnew" install -yq docker-ce kubectl
+
+# Install https://github.com/mikefarah/yq
+sudo snap install yq
