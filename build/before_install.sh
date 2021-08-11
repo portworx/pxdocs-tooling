b0678bfb98ff23488e46c39f5b039a811ef74578
David Su(dsu) <dsu@purestorage.com>
2021-08-11 02:43:42 +0000

    PD-928 Enhance pxdoc-tooling to deploy PR to the staging server

--- build/before_install.sh
+++ build/before_install.sh
@@ -9,7 +9,5 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update -yq
 sudo apt-get -o Dpkg::Options::="--force-confnew" install -yq docker-ce kubectl
 
-# log in to Docker if this isn't a PR
-if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-  echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USER}" --password-stdin
-fi
+# log in to Docker
+echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USER}" --password-stdin
