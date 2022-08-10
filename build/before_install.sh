5ceccbe5198809a0cc9b2c22e2eb4ce124fe7294
nathan-px <53194370+nathan-px@users.noreply.github.com>
2022-08-10 15:44:54 -0700

    add curl to pull version (#117)

--- build/before_install.sh
+++ build/before_install.sh
@@ -13,3 +13,17 @@ sudo apt-get -o Dpkg::Options::="--force-confnew" install -yq docker-ce kubectl
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USER}" --password-stdin
 fi
+
+# try to pull from porx
+
+# Portworx Version
+PXVERSION=2.12.0
+
+# PX BRANCH
+branch=gs-rel-${PXVERSION}
+
+# Get version from the code
+curl https://raw.githubusercontent.com/portworx/porx/${branch}/vendor/github.com/libopenstorage/openstorage/api/server/sdk/api/api.swagger.json\
+		--output api.swagger.json --silent
+ver=$(cat api.swagger.json | jq -r '.info.version')
+echo "Px version v${PXVERSION} on branch ${branch} uses the SDK version v${ver}"
