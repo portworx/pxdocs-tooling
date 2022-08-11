4b090e72ecf4934b66725ee613b9f7681e8e9348
nathan-px <nathaniel.wilson@portworx.com>
2022-08-10 20:35:17 -0700

    experimental data pulling again

--- build/before_install.sh
+++ build/before_install.sh
@@ -16,17 +16,22 @@ fi
 
 # try to pull from porx
 
+
+# this will not work if this is a PR, unless I start sharing encrypted env vars with PRs.   
 if [ -z "$GH_BOT_TOKEN" ] ; then
     echo "You must first get a personal token from github: https://github.com/settings/tokens"
     echo "Then save your personal token in an env var: export GITHUB_PERSONAL_TOKEN=ghp_..."
     exit 1
 fi
 
-# Just pulling this from build.sh for now. We probably want to pull data before the main build script runs.
-export PX_VERSION=$(yq e ".$YAML_SECTION_NAME.LATEST_VERSION" ./themes/pxdocs-tooling/build/products.yaml)
+# Just pulling this from build.sh for now. We probably want to pull data before the main build script runs. This needs more env vars than I'm pulling here.
+# I'm going to bypass this for now.
+
+# export PX_VERSION=$(yq e ".$YAML_SECTION_NAME.LATEST_VERSION" ./themes/pxdocs-tooling/build/products.yaml)
+export PX_VERSION=2.12.0
 
 if [ -z "$PX_VERSION" ] ; then
-    echo "Missing PX_VERSION value"
+    echo "Missing PX_VERSION env var"
     exit 1
 fi
 
