28acb35774123bd5d65d1d57ec4a22ad5f60074a
nathan-px <nathaniel.wilson@portworx.com>
2022-08-10 20:50:11 -0700

    remove experimental changes

--- build/before_install.sh
+++ build/before_install.sh
@@ -13,34 +13,3 @@ sudo apt-get -o Dpkg::Options::="--force-confnew" install -yq docker-ce kubectl
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USER}" --password-stdin
 fi
-
-# try to pull from porx
-
-
-# this will not work if this is a PR, unless I start sharing encrypted env vars with PRs.   
-if [ -z "$GH_BOT_TOKEN" ] ; then
-    echo "You must first get a personal token from github: https://github.com/settings/tokens"
-    echo "Then save your personal token in an env var: export GITHUB_PERSONAL_TOKEN=ghp_..."
-    exit 1
-fi
-
-# Just pulling this from build.sh for now. We probably want to pull data before the main build script runs. This needs more env vars than I'm pulling here.
-# I'm going to bypass this for now.
-
-# export PX_VERSION=$(yq e ".$YAML_SECTION_NAME.LATEST_VERSION" ./themes/pxdocs-tooling/build/products.yaml)
-export PX_VERSION=2.12.0
-
-if [ -z "$PX_VERSION" ] ; then
-    echo "Missing PX_VERSION env var"
-    exit 1
-fi
-
-# PX BRANCH
-branch=gs-rel-${PX_VERSION}
-
-# Get version from the code
-curl -H "Authorization: bearer $GH_BOT_TOKEN" \
-https://raw.githubusercontent.com/portworx/porx/${branch}/vendor/github.com/libopenstorage/openstorage/api/server/sdk/api/api.swagger.json\
-		--output api.swagger.json --silent
-ver=$(cat api.swagger.json | jq -r '.info.version')
-echo "Px version v${PX_VERSION} on branch ${branch} uses the SDK version v${ver}"
