dc149a6ee23c37d7b0d1b60ad75d4e0dedf6af25
nathan-px <nathaniel.wilson@portworx.com>
2022-08-16 11:24:23 -0700

    add echo to see where I am

--- build/main.sh
+++ build/main.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Not sure where we are here
+echo "$PWD"
+
 # This file runs all other shell scripts. It might be a better idea to call these all from travis.
 # The most important thing is that these all run within the same process, so env vars are shared.
 source /themes/pxdocs-tooling/build/set_env_vars.sh
