13ed8380a797aa5797a62b956f5d0e8e0cd1484f
nathan-px <nathaniel.wilson@portworx.com>
2022-08-16 12:55:42 -0700

    try dots instead of source

--- build/main.sh
+++ build/main.sh
@@ -5,6 +5,6 @@ echo "$PWD"
 
 # This file runs all other shell scripts. It might be a better idea to call these all from travis.
 # The most important thing is that these all run within the same process, so env vars are shared.
-source /themes/pxdocs-tooling/build/set_env_vars.sh
-source /themes/pxdocs-tooling/build/pull_data.sh
-source /themes/pxdocs-tooling/build/build.sh
+./themes/pxdocs-tooling/build/set_env_vars.sh
+./themes/pxdocs-tooling/build/pull_data.sh
+./themes/pxdocs-tooling/build/build.sh
