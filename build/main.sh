1b197d163c450b2fd2d5913d511fb29d727fe58a
nathan-px <nathaniel.wilson@portworx.com>
2022-08-16 10:57:10 -0700

    use full paths

--- build/main.sh
+++ build/main.sh
@@ -2,6 +2,6 @@
 
 # This file runs all other shell scripts. It might be a better idea to call these all from travis.
 # The most important thing is that these all run within the same process, so env vars are shared.
-source set_env_vars.sh
-source pull_data.sh
-source build.sh
+source /themes/pxdocs-tooling/build/set_env_vars.sh
+source /themes/pxdocs-tooling/build/pull_data.sh
+source /themes/pxdocs-tooling/build/build.sh
