79a5c97f6633c76c0ebca2dc4d9483fe7d489436
nathan-px <nathaniel.wilson@portworx.com>
2022-08-16 14:23:56 -0700

    gate data pull behind PR

--- build/main.sh
+++ build/main.sh
@@ -4,13 +4,19 @@
 echo "$PWD"
 
 # This file runs all other shell scripts. It might be a better idea to call these all from travis.
-# The most important thing is that these all run within the same process, so env vars are shared.
+# The most important thing is that these all run within the same process, so env vars are shared.``
 
 echo "setting env vars"
 source ./themes/pxdocs-tooling/build/set_env_vars.sh
 
-echo "pulling data for automation"
-source ./themes/pxdocs-tooling/build/pull_data.sh
+# We need to decide if we only want to run this on upstream builds, or also for PRs. We currently don't export ecypted env vars to PRs, so I'll have this exit. 
+if [ "${TRAVIS_PULL_REQUEST}" == "true" ]; then 
+    echo "pulling data for automation"
+    source ./themes/pxdocs-tooling/build/pull_data.sh
+    
+    # we can put any other automation we want to here 
+fi
+
 
 echo "running build"
 source ./themes/pxdocs-tooling/build/build.sh
