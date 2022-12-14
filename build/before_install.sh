027b592e0f5dfa6a182600a9967ce5c2bdd50d11
Charudath Gopal <cgopal@purestorage.com>
2022-12-14 10:59:38 -0800

    Making changes to use 1.25.0 version of Kubectl to workaround Google Auth Issue
     Making changes to use 1.25.0 version of Kubectl to workaround Google Auth Issue

--- build/before_install.sh
+++ build/before_install.sh
@@ -3,11 +3,13 @@
 # update gcloud apt source
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+echo "deb http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 # install docker-ce and kubectl
 sudo apt-get update -yq
-sudo apt-get -o Dpkg::Options::="--force-confnew" install -yq docker-ce kubectl=1.25.0
+sudo apt-get -o Dpkg::Options::="--force-confnew" install -yq docker-ce kubectl
+sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
 
 # log in to Docker if this isn't a PR
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
