302efe86511d7b690e40659ea06d37f6445f1293
Charudath Gopal <cgopal@purestorage.com>
2022-12-14 10:31:02 -0800

    Making changes to use 1.25.0 version of Kubectl to workaround Google Auth issue
    Making changes to use 1.25.0 version of Kubectl to workaround Google Auth issue

--- build/before_install.sh
+++ build/before_install.sh
@@ -7,7 +7,7 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 # install docker-ce and kubectl
 sudo apt-get update -yq
-sudo apt-get -o Dpkg::Options::="--force-confnew" install -yq docker-ce kubectl
+sudo apt-get -o Dpkg::Options::="--force-confnew" install -yq docker-ce kubectl=1.25.0
 
 # log in to Docker if this isn't a PR
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
