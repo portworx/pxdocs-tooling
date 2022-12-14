18d36cd93d8fa699890c06b81f33e024509f1a42
Charudath Gopal <cgopal@purestorage.com>
2022-12-14 11:23:00 -0800

    Making changes to use 1.25.0 version of Kubectl to workaround Google Auth Issue

--- build/before_install.sh
+++ build/before_install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # update gcloud apt source
-export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-echo "deb http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# echo "deb http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+# curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 # install docker-ce and kubectl
 sudo apt-get update -yq
-sudo apt-get -o Dpkg::Options::="--force-confnew" install -yq docker-ce kubectl
-sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
+sudo apt-get -o Dpkg::Options::="--force-confnew" install -yq docker-ce
+sudo snap install kubectl --channel=1.25 --classic
 
 # log in to Docker if this isn't a PR
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
